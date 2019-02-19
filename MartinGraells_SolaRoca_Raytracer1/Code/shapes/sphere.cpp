033f5839f80f8f8fc6701c5138fb971d0299160b
A. Sola Roca <s3834549@fwn-bborg-edu-74-192.fmnsedu.rug.nl>
2019-02-19 18:45:10 +0100

    Added exercice 1 PHONG

---
+++ MartinGraells_SolaRoca_Raytracer1/Code/shapes/sphere.cpp
@@ -0,0 +1,73 @@
+#include "sphere.h"
+
+#include <cmath>
+
+using namespace std;
+
+Hit Sphere::intersect(Ray const &ray)
+{
+    /****************************************************
+    * RT1.1: INTERSECTION CALCULATION
+    *
+    * Given: ray, position, r
+    * Sought: intersects? if true: *t
+    *
+    * Insert calculation of ray/sphere intersection here.
+    *
+    * You have the sphere's center (C) and radius (r) as well as
+    * the ray's origin (ray.O) and direction (ray.D).
+    *
+    * If the ray does not intersect the sphere, return false.
+    * Otherwise, return true and place the distance of the
+    * intersection point from the ray origin in *t (see example).
+    ****************************************************/
+
+    // place holder for actual intersection calculation
+
+    /*Vector OC = (position - ray.O).normalized();
+    if (OC.dot(ray.D) < 0.999) {
+        return Hit::NO_HIT();
+    }
+    double t = 1000;*/
+    
+    float t0,t1;
+    
+    Vector OC = position - ray.O;
+    float tca = OC.dot(ray.D);
+    if ( tca < 0) {
+        return Hit::NO_HIT();
+    }
+    
+    float d2 = OC.dot(OC) - pow(tca,2);
+    if (d2 > pow(r,2)) return Hit::NO_HIT();
+    float thc = sqrt(pow(r,2) - d2);
+    t0 = tca - thc;
+    t1 = tca + thc; 
+    
+    if (t0 > t1) std::swap(t0, t1);
+
+    if (t0 < 0) {
+        t0 = t1; // if t0 is negative, let's use t1 instead
+        if (t0 < 0) return Hit::NO_HIT(); // both t0 and t1 are negative
+    }
+
+    float t = t0; 
+
+    /****************************************************
+    * RT1.2: NORMAL CALCULATION
+    *
+    * Given: t, C, r
+    * Sought: N
+    *
+    * Insert calculation of the sphere's normal at the intersection point.
+    ****************************************************/
+    Vector N = (ray.at(t) - position).normalized();
+
+    return Hit(t,N);
+}
+
+Sphere::Sphere(Point const &pos, double radius)
+:
+    position(pos),
+    r(radius)
+{}
